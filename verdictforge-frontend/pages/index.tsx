d796d5af80e1246e65d2e8624bee92277fdf7be7
ParthivmMS <parthivmssince2005@gmail.com>
2025-06-14 23:16:34 +0530

    Create index.tsx

---
+++ verdictforge-frontend/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <a href="/summarizer" className="text-blue-600 underline text-xl">Go to Judgment Summarizer</a>
+    </main>
+  );
+}
